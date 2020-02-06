09610635bdd66862211c1df4c3c0b3356e495084
dani herrera <ctrl.alt.d@gmail.com>
2020-02-06 15:46:04 +0100

    finished

--- demo/Data/IntervalData.cs
+++ demo/Data/IntervalData.cs
@@ -1,15 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace demo.Data
 {
-    public class IntervalData
+    public class Starship
     {
-        public DateTime Date { get; set; }
+        [Required]
+        [StringLength(16, ErrorMessage = "Identifier too long (16 character limit).")]
+        public string Identifier { get; set; }
 
-        public int TemperatureC { get; set; }
+        public string Description { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        [Required]
+        public string Classification { get; set; }
 
-        public string Summary { get; set; }
+        [Range(1, 100000, ErrorMessage = "Accommodation invalid (1-100000).")]
+        public int MaximumAccommodation { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true",
+            ErrorMessage = "This form disallows unapproved ships.")]
+        public bool IsValidatedDesign { get; set; }
+
+        [Required]
+        public DateTime ProductionDate { get; set; }
     }
 }
